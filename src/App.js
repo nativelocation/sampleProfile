42602a074176cca3965f74d345c67f32d77382e4
nativelocation <bluesky6460@mail.com>
2019-05-09 03:16:38 -0500

    update profile

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import logo from './assets/img/person.png';
+import heart from './assets/img/heart.png';
+import heartfill from './assets/img/heartfill.png';
+import star from './assets/img/star.png';
+import starfill from './assets/img/starfill.png';
 import './App.css';
 
 class App extends Component {
@@ -20,8 +24,10 @@ class App extends Component {
             <div className="profile-container">
               <div className="profile-header">
                 Developer Profile
-                {heartState && <i className="far fa-heart" onClick={this.onHeartClick}></i>}
-                {!heartState && <i className="fas fa-heart" onClick={this.onHeartClick} style={{ color: 'red' }}></i>}
+                {/* {heartState && <i className="far fa-heart" onClick={this.onHeartClick}></i>}
+                {!heartState && <i className="fas fa-heart" onClick={this.onHeartClick} style={{ color: 'red' }}></i>} */}
+                {heartState && <img src={heart} onClick={this.onHeartClick} alt="heart" />}
+                {!heartState && <img src={heartfill} onClick={this.onHeartClick} alt="heartfill" />}
               </div>
               <div className="profile-body">
                 <img src={logo} className="profile-avatar" alt="logo" />
@@ -40,11 +46,11 @@ class App extends Component {
                 </div>
                 <div className="profile-rateviews">
                   <div className="profile-rateviews-number">
-                    <i className="fa fa-star"></i>
-                    <i className="fa fa-star"></i>
-                    <i className="fa fa-star"></i>
-                    <i className="fa fa-star"></i>
-                    <i className="fa fa-star" style={{ opacity: 0.3 }}></i>
+                    <img src={starfill} alt="starfill" />
+                    <img src={starfill} alt="starfill" />
+                    <img src={starfill} alt="starfill" />
+                    <img src={starfill} alt="starfill" />
+                    <img src={star} alt="star" />
                   </div>
                   <div className="profile-rateviews-desc">Avg Rating</div>
                 </div>
